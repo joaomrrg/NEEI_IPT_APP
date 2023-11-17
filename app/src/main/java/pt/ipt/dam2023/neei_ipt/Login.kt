51904c1d6b44f5dca96f2bcf6acc4d4798a5c1d5
joaom <joaom@DESKTOP-PNCVF6D>
2023-11-17 15:54:40 +0000

    Splash animation Created Login Page Created Side menu Nav Bar Created

--- app/src/main/java/pt/ipt/dam2023/neei_ipt/Login.kt
+++ app/src/main/java/pt/ipt/dam2023/neei_ipt/Login.kt
@@ -1,11 +1,27 @@
 package pt.ipt.dam2023.neei_ipt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registarLabel = findViewById<TextView>(R.id.registar_label)
+        val recuperarLabel = findViewById<TextView>(R.id.recuperar_label)
+
+        registarLabel.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        recuperarLabel.setOnClickListener{
+            val intent = Intent(this, RecuperarPass::class.java)
+            startActivity(intent)
+        }
     }
+
 }
