ae80c00cb0e9a1ddf0dd8691cb0cda2b0decabcb
joaom <joaom@DESKTOP-PNCVF6D>
2023-11-18 15:25:44 +0000

    Splash animation Created Login Page Created Side menu Nav Bar Created Registration Page Created Forgot Password Page Created Nav Bar Created

---
+++ app/src/main/java/pt/ipt/dam2023/neei_ipt/RecuperarPass.kt
@@ -0,0 +1,11 @@
+package pt.ipt.dam2023.neei_ipt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecuperarPass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recup_pass)
+    }
+}
