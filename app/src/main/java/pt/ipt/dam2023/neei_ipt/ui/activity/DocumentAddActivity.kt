21c1c1ce10fb6f84388cb48b6a58f61cd2801292
joaom <joaom@DESKTOP-PNCVF6D>
2023-12-27 00:03:27 +0000

    Add document interface

---
+++ app/src/main/java/pt/ipt/dam2023/neei_ipt/ui/activity/DocumentAddActivity.kt
@@ -0,0 +1,12 @@
+package pt.ipt.dam2023.neei_ipt.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.ipt.dam2023.neei_ipt.R
+
+class DocumentAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_document_add)
+    }
+}
