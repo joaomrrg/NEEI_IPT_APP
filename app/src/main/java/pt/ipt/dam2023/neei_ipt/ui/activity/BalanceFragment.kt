584338f6cc534a65b23c3ccbac3d4e24712183a7
Gonçalo Costa <gfmcosta03@gmail.com>
2024-01-06 18:27:27 +0000

    getTransactionList in Balance view Fixing some updateProfile bugs

--- app/src/main/java/pt/ipt/dam2023/neei_ipt/ui/activity/BalanceFragment.kt
+++ app/src/main/java/pt/ipt/dam2023/neei_ipt/ui/activity/BalanceFragment.kt
@@ -4,14 +4,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import pt.ipt.dam2023.neei_ipt.R
+import pt.ipt.dam2023.neei_ipt.model.Document
+import pt.ipt.dam2023.neei_ipt.model.Transaction
 import pt.ipt.dam2023.neei_ipt.model.TransactionBudget
 import pt.ipt.dam2023.neei_ipt.model.User
 import pt.ipt.dam2023.neei_ipt.retrofit.RetrofitInitializer
+import pt.ipt.dam2023.neei_ipt.ui.adapter.DocumentAdapter
+import pt.ipt.dam2023.neei_ipt.ui.adapter.TransactionAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,7 +25,7 @@ class BalanceFragment : Fragment() {
 
     // Saldo atual simulado
     private var currentBalance = "-"
-
+    private lateinit var listView: ListView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,7 +33,7 @@ class BalanceFragment : Fragment() {
     ): View? {
         // Infla o layout para este fragmento
         val view = inflater.inflate(R.layout.balance_layout, container, false)
-
+        listView = view.findViewById(R.id.transactionsListView)
         // Encontra as views no layout inflado
         val currentBalanceText = view.findViewById<TextView>(R.id.currentBalanceText)
         val hideButton = view.findViewById<ImageView>(R.id.hideBttn)
@@ -37,6 +42,14 @@ class BalanceFragment : Fragment() {
         val checkMovementButton = view.findViewById<FloatingActionButton>(R.id.checkMovementBttn)
         val euroText = view.findViewById<TextView>(R.id.euro)
 
+        getTransactions { result ->
+            if (result != null) {
+                val transactionList = result.filterNotNull() // Filtrar documentos nulos
+                // Inicializar a ListView e configurar o adaptador
+                val adapter = TransactionAdapter(requireContext(), R.layout.item_transaction, transactionList)
+                listView.adapter = adapter
+            }
+        }
 
         getBudget { result ->
             if (result != null) {
@@ -91,6 +104,25 @@ class BalanceFragment : Fragment() {
 
             override fun onFailure(call: Call<TransactionBudget?>, t: Throwable) {
                 // Em caso de falha na requisição, registra o erro no Log
+                t.message?.let { Log.e("Erro onFailure", it) }
+            }
+        })
+    }
+
+    /**
+     * Função para listar todas as transações do NEEI
+     */
+    private fun getTransactions(onResult: (List<Transaction?>) -> Unit) {
+        val call = RetrofitInitializer().APIService().listTransactions()
+        call.enqueue(object : Callback<List<Transaction>?> {
+            override fun onResponse(call: Call<List<Transaction>?>?, response: Response<List<Transaction>?>?) {
+                response?.body()?.let {
+                    val transactions: List<Transaction?> = it
+                    onResult(transactions)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Transaction>?>, t: Throwable) {
                 t.message?.let { Log.e("Erro onFailure", it) }
             }
         })
---
+++ app/src/main/java/pt/ipt/dam2023/neei_ipt/ui/adapter/TransactionAdapter.kt
@@ -0,0 +1,53 @@
+package pt.ipt.dam2023.neei_ipt.ui.adapter
+
+import android.Manifest
+import android.content.Context
+import android.os.Environment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentActivity
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import pt.ipt.dam2023.neei_ipt.R
+import pt.ipt.dam2023.neei_ipt.model.Document
+import pt.ipt.dam2023.neei_ipt.model.Transaction
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Scanner
+
+class TransactionAdapter(context: Context, resource: Int, objects: List<Transaction>) :
+
+    ArrayAdapter<Transaction>(context, resource, objects){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val itemView = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.item_transaction, parent, false)
+
+        val transaction = getItem(position)
+        val descriptionTextView = itemView.findViewById<TextView>(R.id.transactionDescription)
+        val dateTextView = itemView.findViewById<TextView>(R.id.transactionDate)
+        val valueTextView = itemView.findViewById<TextView>(R.id.transactionValue)
+        descriptionTextView.text = transaction?.description
+        val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
+        val formattedDate = dateFormatter.format(transaction?.date)
+        dateTextView.text = formattedDate
+        valueTextView.text = transaction?.value.toString()+"€"
+        return itemView
+    }
+}
